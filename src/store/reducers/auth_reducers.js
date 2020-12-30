5d59b49d88c287bb687c9324a3b6ce31f1bb6d13
lopeselio <lopeselio@gmail.com>
2020-12-31 00:38:32 +0530

    reducer

---
+++ src/store/reducers/auth_reducers.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+    user: [], 
+    isAuth: false,
+    error: null
+}
+
+export default function(state=INITIAL_STATE, action) {
+    switch(action.type){
+        default:
+            return state
+    }
+}
