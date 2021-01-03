02a551a0a94bed708c3ccbedb092b7bffa7e56a7
lopeselio <lopeselio@gmail.com>
2021-01-03 18:10:13 +0530

    video player screen

---
+++ src/components/home/videos/video/index.js
@@ -0,0 +1,14 @@
+ 
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const VideoScreen = () => {
+    return(
+        <View>
+            <Text>Video screen</Text>
+        </View>
+    )
+}
+
+export default VideoScreen;
