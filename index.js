1c7a9043e1b9e57a006c5e2de5515176caa5c5b6
lopeselio <lopeselio@gmail.com>
2021-01-24 15:28:35 +0530

    main app file

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// import { registerRootComponent } from 'expo';
-// import React from 'react';
-// import App from './src/App';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware, compose } from 'redux'
-// import promiseMiddleware from 'redux-promise';
-// import reducers from './src/store/reducers';
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const createStoreWithMiddleware = createStore(
-//     reducers,
-//     composeEnhancers(applyMiddleware(promiseMiddleware))
-// )
-
-// const reduxApp = () => (
-//     <Provider store={createStoreWithMiddleware}>
-//         <App/>
-//     </Provider>
-// )
-
-// // registerRootComponent calls AppRegistry.registerComponent('main', () => reduxApp);
-// // It also ensures that whether you load the app in the Expo client or in a native build,
-// // the environment is set up appropriately
-// registerRootComponent(reduxApp);
-
 import React from 'react'
 import {AppRegistry} from 'react-native';
 import App from './src/App';
