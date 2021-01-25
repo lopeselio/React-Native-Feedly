2195958c4d428b52e6499ba6b8113719b83b04a1
lopeselio <lopeselio@gmail.com>
2021-01-25 16:54:15 +0530

    video player youtube

--- src/components/home/videos/video/index.js
+++ src/components/home/videos/video/index.js
@@ -1,14 +1,24 @@
- 
 import React from 'react';
 import { View, Text, ScrollView } from 'react-native';
-import ContentShow from '../../../../utils/contentShow'
-
+import ContentShow from '../../../../utils/contentShow';
+import Youtube from 'react-native-youtube';
 
 const VideoScreen = () => {
     return(
         <ScrollView>
             <View>
-                <ContentShow />
+                <Youtube
+                    apiKey="AIzaSyCCn-f3qw2j65uvqb9bcxg2T-26XpDWq70"
+                    videoId="rmJmypWeGS4"
+                    play={false}
+
+                    onReady={ e => console.log('ready')}
+                    onChangeState={ e => console.log(e)}
+                    onError={error => console.log(error)}
+
+                    style={{alignSelf:'stretch',height:300}}
+                />
+                <ContentShow/>     
             </View>
         </ScrollView>
     )
