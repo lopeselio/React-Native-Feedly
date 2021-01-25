b924dc0bd19ad437f00bb82e4cbe7e4331a2d8d0
lopeselio <lopeselio@gmail.com>
2021-01-25 10:09:35 +0530

    creating a videos component

--- src/components/home/videos/index.js
+++ src/components/home/videos/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
-
+import { View, Text, Button, ScrollView, ActivityIndicator, StyleSheet } from 'react-native';
+import { Tile } from 'react-native-elements'
 
 const VideosScreen = ({navigation}) => {
+    const renderVideos = () => (
+        <Tile 
+            imageSrc={{uri:'https://picsum.photos/200/300'}}
+            title='Deserunt culpa enim enim aliquip irure magna aliquip culpa'
+            icon = {{name:'play-circle', type: 'font-awesome', color:'#fff', size:50 }}
+            contentContainerStyle={StyleSheet.contentContainerStyle}
+            containerStyle={styles.containerStyle}
+            titleStyle={{fontSize: 15}}
+        />
+    )
+
     return(
-        <View>
-            <Text>Videos</Text>
-            <Button
-                title="see article"
-                onPress={()=> navigation.navigate('Video_screen')}
-            />
-        </View>
+        <ScrollView>
+            <View>
+                {renderVideos()}
+            </View>
+        </ScrollView>
+        
     )
 }
+
+const styles = StyleSheet.create({
+    contentContainerStyle: {
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: '#e1e8ee',
+        shadowColor: 'rgba(0,0,0,.2)'
+    },
+    containerStyle: {
+        width: '100%',
+        height: 250,
+        marginBottom: 15
+    }
+})
 
 export default VideosScreen;
