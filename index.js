ea62afb88c58a1e54f328de7aa11eb98e9290c2a
lopeselio <lopeselio@gmail.com>
2021-01-27 13:59:15 +0530

    importing toats in app file

--- index.js
+++ index.js
@@ -8,21 +8,30 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import promiseMiddleware from 'redux-promise'
 import reducers from './src/store/reducers'
 import { Provider as PaperProvider } from 'react-native-paper'
+import Toast from 'react-native-toast-message';
+import {View,Text} from 'react-native';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const createStoreWithMiddleware = createStore(
     reducers,
     composeEnhancers(applyMiddleware(promiseMiddleware))
 )
-const appRedux = () => {
-    return(
-        <Provider store={createStoreWithMiddleware}>
-            <PaperProvider>
-                <App />
-            </PaperProvider>
-        </Provider>
-    )
-    
+
+const toastConfig = {
+    info: (internalState) => (
+      <View style={{ height: 60, width: '100%', backgroundColor: 'pink' }}>
+        <Text>{internalState.text1}</Text>
+      </View>
+    )   
 }
+
+const appRedux = () => (
+    <Provider store={createStoreWithMiddleware}>
+        <PaperProvider>
+            <App/>
+            <Toast config={toastConfig} ref={(ref) => Toast.setRef(ref)} />
+        </PaperProvider>
+    </Provider>
+)
 
 AppRegistry.registerComponent(appName, () => appRedux);
