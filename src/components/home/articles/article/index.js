94c6a10bfa548067afb4b36623c3808e73034d37
lopeselio <lopeselio@gmail.com>
2021-02-03 14:33:27 +0530

    logout

--- src/components/home/articles/article/index.js
+++ src/components/home/articles/article/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { View, Text, ScrollView, ActivityIndicator } from 'react-native';
 import { Image } from 'react-native-elements'
-import ContentShow from '../../../../utils/contentShow'
+import ContentShow from '../../../../utils/contentShow';
 
 const ArticleScreen = () => {
     return(
         <ScrollView>
             <View>
                 <Image 
-                    src={{url: 'https://picsum.photos/200/300'}}
-                    style={{width: '100%', height: 200}}
-                    PlaceholderContent={<ActivityIndicator />}
-                />
-                <ContentShow />
+                    source={{uri:'https://picsum.photos/200/300'}}
+                    style={{width:'100%', height:200}}
+                    PlaceholderContent={<ActivityIndicator/>}
+                />     
+                <ContentShow/>           
             </View>
         </ScrollView>
     )
